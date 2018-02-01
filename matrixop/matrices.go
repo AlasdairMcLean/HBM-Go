3ac3c47bcd6b7f4677c5d0fdeb8868a74e2d162e
AlasdairMcLean <alasdair.mclean@macaulay.cuny.edu>
2018-02-01 18:42:16 -0500

    Added matrix struct
    +Added methods for scalar multiplication of matrices
    
    +Added a method for construction of a new matrix for preallocation

---
+++ matrixop/matrices.go
@@ -0,0 +1,28 @@
+package matrixop
+
+//Matrix is a struct to streamline matrix operations
+type Matrix struct {
+	rows int
+	cols int
+	data [][]int
+}
+
+//NewMatrix constructs a Matrix structure with specified rows and columns, returning the pointer to the new matrix
+func NewMatrix(rows, cols int) *Matrix {
+	ndata := make([][]int, rows)
+	for i := 0; i < rows; i++ {
+		ndata[i] = make([]int, cols)
+	}
+	newmat := Matrix{rows, cols, ndata}
+	return &newmat
+}
+
+//STimes performs simple scalar matrix multiplication
+func STimes(m1 Matrix, s int) Matrix {
+	for i := 0; i < m1.rows; i++ {
+		for j := 0; j < m1.cols; j++ {
+			m1.data[i][j] *= s
+		}
+	}
+	return m1
+}
