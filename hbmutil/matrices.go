40d552f688d401ee66f033914d9264e95e807c3c
AlasdairMcLean <alasdair.mclean@macaulay.cuny.edu>
2018-02-01 18:58:44 -0500

    Added ones and zeros function

--- hbmutil/matrices.go
+++ hbmutil/matrices.go
@@ -17,6 +17,36 @@ func NewMatrix(rows, cols int) *Matrix {
 	return &newmat
 }
 
+//MatZeros replicates matlab's zeros() function. Returns a new matrix full of 0's
+func MatZeros(rows, cols int) *Matrix {
+	ndata := make([][]int, rows)
+	for i := 0; i < rows; i++ {
+		ndata[i] = make([]int, cols)
+	}
+	for j := 0; j < rows; j++ {
+		for i := 0; i < cols; i++ {
+			ndata[j][i] = 0
+		}
+	}
+	newmat := Matrix{rows, cols, ndata}
+	return &newmat
+}
+
+//MatOnes replicates matlab's ones() function. Returns a new matrix full of 1's
+func MatOnes(rows, cols int) *Matrix {
+	ndata := make([][]int, rows)
+	for i := 0; i < rows; i++ {
+		ndata[i] = make([]int, cols)
+	}
+	for j := 0; j < rows; j++ {
+		for i := 0; i < cols; i++ {
+			ndata[j][i] = 1
+		}
+	}
+	newmat := Matrix{rows, cols, ndata}
+	return &newmat
+}
+
 //STimes performs simple scalar matrix multiplication
 func STimes(m1 Matrix, s int) Matrix {
 	for i := 0; i < m1.rows; i++ {
