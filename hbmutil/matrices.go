4a3a52ed7cc23652e25f5b40350cb9cb97b0b649
AlasdairMcLean <alasdair.mclean@macaulay.cuny.edu>
2018-02-03 11:17:30 -0500

    Added additional matrix types

--- hbmutil/matrices.go
+++ hbmutil/matrices.go
@@ -1,24 +1,71 @@
 package hbmutil
 
-//Matrix is a struct to streamline matrix operations
-type Matrix struct {
+//Matrix is an overarching interface for the specific matrices to be derived from allowing overloading non-type specific methods like scalar multiplication
+type Matrix interface {
+	STimes()
+}
+
+//Matrixi is an 8bit unsigned integer struct to streamline matrix operations
+type Matrixi struct {
 	rows int
 	cols int
 	data [][]int
 }
 
-//NewMatrix constructs a Matrix structure with specified rows and columns, returning the pointer to the new matrix
-func NewMatrix(rows, cols int) *Matrix {
+//Matrixf is a 32bit floating point struct to streamline matrix operations
+type Matrixf struct {
+	rows int
+	cols int
+	data [][]float32
+}
+
+//Matrixff is a 64bit floating point struct to streamline matrix operations
+type Matrixff struct {
+	rows int
+	cols int
+	data [][]float64
+}
+
+//
+// Matrix preallocation implementations
+//
+
+//NewMatrixi constructs a integer containing Matrix structure with specified rows and columns, returning the pointer to the new matrix
+func NewMatrixi(rows, cols int) *Matrixi {
 	ndata := make([][]int, rows)
 	for i := 0; i < rows; i++ {
 		ndata[i] = make([]int, cols)
 	}
-	newmat := Matrix{rows, cols, ndata}
+	newmat := Matrixi{rows, cols, ndata}
 	return &newmat
 }
 
-//MatZeros replicates matlab's zeros() function. Returns a new matrix full of 0's
-func MatZeros(rows, cols int) *Matrix {
+//NewMatrixf constructs a Matrix structure with specified rows and columns, returning the pointer to the new 32bit floating point matrix
+func NewMatrixf(rows, cols int) *Matrixf {
+	ndata := make([][]float32, rows)
+	for i := 0; i < rows; i++ {
+		ndata[i] = make([]float32, cols)
+	}
+	newmat := Matrixf{rows, cols, ndata}
+	return &newmat
+}
+
+//NewMatrixff constructs a Matrix structure with specified rows and columns, returning the pointer to the new 64bit floating point matrix
+func NewMatrixff(rows, cols int) *Matrixff {
+	ndata := make([][]float64, rows)
+	for i := 0; i < rows; i++ {
+		ndata[i] = make([]float64, cols)
+	}
+	newmat := Matrixff{rows, cols, ndata}
+	return &newmat
+}
+
+//
+// Implementation of Matlab's zeros() function
+//
+
+//MatZerosi replicates matlab's zeros() function. Returns a new integer matrix full of 0's
+func MatZerosi(rows, cols int) *Matrixi {
 	ndata := make([][]int, rows)
 	for i := 0; i < rows; i++ {
 		ndata[i] = make([]int, cols)
@@ -28,12 +75,46 @@ func MatZeros(rows, cols int) *Matrix {
 			ndata[j][i] = 0
 		}
 	}
-	newmat := Matrix{rows, cols, ndata}
+	newmat := Matrixi{rows, cols, ndata}
 	return &newmat
 }
 
-//MatOnes replicates matlab's ones() function. Returns a new matrix full of 1's
-func MatOnes(rows, cols int) *Matrix {
+//MatZerosf replicates matlab's zeros() function. Returns a new 32bit floating point matrix full of 0's
+func MatZerosf(rows, cols int) *Matrixf {
+	ndata := make([][]float32, rows)
+	for i := 0; i < rows; i++ {
+		ndata[i] = make([]float32, cols)
+	}
+	for j := 0; j < rows; j++ {
+		for i := 0; i < cols; i++ {
+			ndata[j][i] = 0
+		}
+	}
+	newmat := Matrixf{rows, cols, ndata}
+	return &newmat
+}
+
+//MatZerosff replicates matlab's zeros() function. Returns a new 64bit floating point matrix full of 0's
+func MatZerosff(rows, cols int) *Matrixff {
+	ndata := make([][]float64, rows)
+	for i := 0; i < rows; i++ {
+		ndata[i] = make([]float64, cols)
+	}
+	for j := 0; j < rows; j++ {
+		for i := 0; i < cols; i++ {
+			ndata[j][i] = 0
+		}
+	}
+	newmat := Matrixff{rows, cols, ndata}
+	return &newmat
+}
+
+//
+// Implementation of Matlab's ones() function:
+//
+
+//MatOnesi replicates matlab's ones() function. Returns a new integer matrix full of 1's
+func MatOnesi(rows, cols int) *Matrixi {
 	ndata := make([][]int, rows)
 	for i := 0; i < rows; i++ {
 		ndata[i] = make([]int, cols)
@@ -43,12 +124,66 @@ func MatOnes(rows, cols int) *Matrix {
 			ndata[j][i] = 1
 		}
 	}
-	newmat := Matrix{rows, cols, ndata}
+	newmat := Matrixi{rows, cols, ndata}
 	return &newmat
 }
 
-//STimes performs simple scalar matrix multiplication
-func STimes(m1 Matrix, s int) Matrix {
+//MatOnesf replicates matlab's ones() function. Returns a new 32bit floating point matrix full of 1's
+func MatOnesf(rows, cols int) *Matrixf {
+	ndata := make([][]float32, rows)
+	for i := 0; i < rows; i++ {
+		ndata[i] = make([]float32, cols)
+	}
+	for j := 0; j < rows; j++ {
+		for i := 0; i < cols; i++ {
+			ndata[j][i] = 1
+		}
+	}
+	newmat := Matrixf{rows, cols, ndata}
+	return &newmat
+}
+
+//MatOnesff replicates matlab's ones() function. Returns a new 64bit floating point matrix full of 1's
+func MatOnesff(rows, cols int) *Matrixff {
+	ndata := make([][]float64, rows)
+	for i := 0; i < rows; i++ {
+		ndata[i] = make([]float64, cols)
+	}
+	for j := 0; j < rows; j++ {
+		for i := 0; i < cols; i++ {
+			ndata[j][i] = 1
+		}
+	}
+	newmat := Matrixff{rows, cols, ndata}
+	return &newmat
+}
+
+//
+// Scalar multiplication implementation
+//
+
+//STimes performs simple scalar matrix multiplication for uint8 matrices
+func (m1 Matrixi) STimes(s int) Matrixi {
+	for i := 0; i < m1.rows; i++ {
+		for j := 0; j < m1.cols; j++ {
+			m1.data[i][j] *= s
+		}
+	}
+	return m1
+}
+
+//STimes performs simple scalar matrix multiplication for float32 matrices
+func (m1 Matrixf) STimes(s float32) Matrixf {
+	for i := 0; i < m1.rows; i++ {
+		for j := 0; j < m1.cols; j++ {
+			m1.data[i][j] *= s
+		}
+	}
+	return m1
+}
+
+//STimes performs simple scalar matrix multiplication for float32 matrices
+func (m1 Matrixff) STimes(s float64) Matrixff {
 	for i := 0; i < m1.rows; i++ {
 		for j := 0; j < m1.cols; j++ {
 			m1.data[i][j] *= s
