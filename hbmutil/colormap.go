077b127b32afc652e49ca8eb5f987df42870fa09
AlasdairMcLean <alasdair.mclean@macaulay.cuny.edu>
2018-02-06 09:28:44 -0500

    Added in-line commenting for colormap.go

--- hbmutil/colormap.go
+++ hbmutil/colormap.go
@@ -1,13 +1,13 @@
 package hbmutil
 
-//Gscmap returns an N by 3 matrix with resultant r,g,b values for (vals) discrete levels. Similar to Matlab's 'gray()' command.
+//Gscmap returns a float32 N by 3 matrix with resultant r,g,b values for (vals) discrete levels. Similar to Matlab's 'gray()' command.
+//Thus, Gscmap will return a (12*n) byte matrix.
 func Gscmap(vals int) *Matrixf {
-	resultantcmap := *NewMatrixf(vals, 3)
-	var maxval float32 = 1.0
-	level := maxval / (float32(vals) - 1)
-	for i := 0; i < vals; i++ {
-		resultantcmap.Data[vals-i-1] = []float32{level * float32(i), level * float32(i), level * float32(i)}
+	resultantcmap := *NewMatrixf(vals, 3) //create the n x 3 colormap rgb matrix to be returned and dereference it.
+	level := 1.0 / (float32(vals) - 1)    //determine the increments necessary to create a clean transition between inclusive 0 and 1
+	for i := 0; i < vals; i++ {           //iterate through each value of the colormap
+		resultantcmap.Data[vals-i-1] = []float32{level * float32(i), level * float32(i), level * float32(i)} // since this colormap is greyscale, all values will be the same per row.
 	}
-	resultantcmap = resultantcmap.STimes(float32(255))
-	return &resultantcmap
+	resultantcmap = resultantcmap.STimes(float32(255)) //Finally, scalar multiply each element by 255 so that the resulting array fits the standard 32B dynamic range [0,255]
+	return &resultantcmap                              //return the pointer to the new matrix
 }
